c3e1f5eda433760a02c72b0f69cf3de0f40059c9
rajibrahman74e <rajiburrahman74@gmail.com>
2023-04-05 17:07:55 +0600

    [ADD]-set-card-details-in-book-components

---
+++ src/components/Book.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Book = ({book}) => {
+  return (
+    <div className="overflow-hidden relative transition duration-200 translate hover:-translate-y-2 rounded shadow-lg hover:shadow-2xl">
+      <img
+        src={book.image}
+        className="object-cover w-full h-56 md:h-64 xl:h-80"
+        alt="book cover page"
+      />
+
+      <div className="bg-black px-6 py-4 flex flex-col text-gray-300 bg-opacity-75 opacity-0 hover:opacity-100 absolute inset-0 transition-opacity duration-200">
+        <p>{book.title}</p>
+        <br />
+        <p>{book.subtitle.substring(0, 45)}...</p>
+        <br />
+        <p className="mt-auto">Price: {book.price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import Book from "./Book";
 
 const Books = () => {
   const { books } = useLoaderData();
@@ -9,21 +10,7 @@ const Books = () => {
     <div className="my-container">
       <div className="grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2 ">
         {books.map((book) => (
-          <div className="overflow-hidden relative transition duration-200 translate hover:-translate-y-2 rounded shadow-lg hover:shadow-2xl">
-            <img
-              src={book.image}
-              className="object-cover w-full h-56 md:h-64 xl:h-80"
-              alt="book cover page"
-            />
-
-            <div className="bg-black px-6 py-4 flex flex-col text-gray-300 bg-opacity-75 opacity-0 hover:opacity-100 absolute inset-0 transition-opacity duration-200">
-              <p>{book.title}</p>
-              <br />
-              <p>{book.subtitle.substring(0, 45)}...</p>
-              <br />
-              <p className="mt-auto">Price: {book.price}</p>
-            </div>
-          </div>
+          <Book key={book.id} book={book} />
         ))}
       </div>
     </div>
