e9e12117005967ac7488340b95da90982448b816
rajibrahman74e <rajiburrahman74@gmail.com>
2023-04-06 12:09:23 +0600

    [ADD]-footer-complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
-      <Header/>
-      <Outlet/>
+      <Header />
+      <div className="min-h-[calc(100vh-136px)]">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
